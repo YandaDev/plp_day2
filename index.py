254c6e549e6e64b4a9be5f4bdf651438e3b02bf2
YandaDev <nhlebelasn@protonmail.com>
2025-02-25 14:51:17 +0200

    added comments

--- index.py
+++ index.py
@@ -1,2 +1,5 @@
+#ask user for their name and strip any whitespace and  capitalize the first letter of each word
 name = input("what's your name? ").strip().title()
+
+#say hello to the user
 print(f"Hello, {name}!")
