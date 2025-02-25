ff4a94485d3004856d4b50987f265a07ae6b46f8
YandaDev <nhlebelasn@protonmail.com>
2025-02-25 14:55:30 +0200

    added comments

--- today.py
+++ today.py
@@ -1,6 +1,9 @@
+#ask user to input a number 
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
+#multiply x and y and round the result to 3 decimal places
 z = round(x * y, 3)
 
+#display the result and format it with commas
 print(f"{z:,}")
